03e2bc7de7bad6156dc1d4e520b83d2c4de94590
Jonathan Strong <142829586+thejoeman23@users.noreply.github.com>
2025-12-01 15:59:26 -0500

    Added Singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
